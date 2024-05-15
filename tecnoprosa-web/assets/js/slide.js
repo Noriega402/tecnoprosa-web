708d78e98ef05948a21a2be5e6ea98afcfbac9b7
Noriega402 <d.noriegachajon@gmail.com>
2024-05-15 00:28:32 -0600

    agregar mapa actualizado y footer

--- tecnoprosa-web/assets/js/slide.js
+++ tecnoprosa-web/assets/js/slide.js
@@ -1,21 +1,6 @@
-const slides = document.querySelectorAll('.slider img');
-let currentSlide = 0;
-
-function showSlide(index) {
-  // Oculta todas las imágenes
-  slides.forEach(slide => {
-    slide.classList.remove('active');
+jQuery(document).ready(function ($) {
+  $(".slider-img").on("click", function () {
+      $(".slider-img").removeClass("active");
+      $(this).addClass("active");
   });
-  // Muestra la imagen actual
-  slides[index].classList.add('active');
-}
-
-function nextSlide() {
-  currentSlide = (currentSlide + 1) % slides.length;
-  showSlide(currentSlide);
-}
-
-// Mostrar la primera imagen
-showSlide(currentSlide);
-// Cambiar a la siguiente imagen cada 5 segundos
-setInterval(nextSlide, 5000);
+});
